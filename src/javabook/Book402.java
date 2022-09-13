b4ffc15cc36628895d2e62ab7ac0bdb98a35d36a
bbyya551 <abe109yd576@gmail.com>
2022-09-14 00:03:16 +0900

    [Add]414

---
+++ src/javabook/Book402.java
@@ -0,0 +1,8 @@
+package javabook;
+
+public class Book402 {
+	public static void main(String[] args) {
+		int[] scores;
+		scores = new int[5];
+	}
+}
---
+++ src/javabook/Book407.java
@@ -0,0 +1,8 @@
+package javabook;
+
+public class Book407 {
+	public static void main(String[] args) {
+		int[] scores = new int[5];
+		System.out.println(scores[0]);
+	}
+}
